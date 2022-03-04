f8a5ac1ce042076f27ffee8612ff23ce86a8e275
Lucas Gabriel <66486119+lucasgabriel07@users.noreply.github.com>
2022-03-04 18:13:49 -0300

    Renomeando variáveis css

--- js/theme.js
+++ js/theme.js
@@ -6,9 +6,9 @@ function changeTheme(checkbox) {
   
   if (checkbox.checked) {
     // Dark
-    root.style.setProperty('--bg_color_1', getComputedStyle(root).getPropertyValue('--bg_dark_1'));
-    root.style.setProperty('--bg_color_2', getComputedStyle(root).getPropertyValue('--bg_dark_2'));
-    root.style.setProperty('--fg_color', getComputedStyle(root).getPropertyValue('--fg_dark'));
+    root.style.setProperty('--bg-color-1', getComputedStyle(root).getPropertyValue('--bg-dark-1'));
+    root.style.setProperty('--bg-color-2', getComputedStyle(root).getPropertyValue('--bg-dark-2'));
+    root.style.setProperty('--fg-color', getComputedStyle(root).getPropertyValue('--fg_dark'));
 
     arrows.forEach(arrow => {
       arrow.src = 'assets/white_arrow.svg';
@@ -19,9 +19,9 @@ function changeTheme(checkbox) {
   
   else {
     // Light
-    root.style.setProperty('--bg_color_1', getComputedStyle(root).getPropertyValue('--bg_light_1'));
-    root.style.setProperty('--bg_color_2', getComputedStyle(root).getPropertyValue('--bg_light_2'));
-    root.style.setProperty('--fg_color', getComputedStyle(root).getPropertyValue('--fg_light'));
+    root.style.setProperty('--bg-color-1', getComputedStyle(root).getPropertyValue('--bg-light-1'));
+    root.style.setProperty('--bg-color-2', getComputedStyle(root).getPropertyValue('--bg-light-2'));
+    root.style.setProperty('--fg-color', getComputedStyle(root).getPropertyValue('--fg-light'));
     
     arrows.forEach(arrow => {
       arrow.src = 'assets/black_arrow.svg';

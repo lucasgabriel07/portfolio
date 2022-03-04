9b1fecd3c16b6f41236fa64383d952383866f9d2
Lucas Gabriel <66486119+lucasgabriel07@users.noreply.github.com>
2022-03-04 01:58:16 -0300

    Melhorando temas

--- js/theme.js
+++ js/theme.js
@@ -6,8 +6,9 @@ function changeTheme(checkbox) {
   
   if (checkbox.checked) {
     // Dark
-    root.style.setProperty('--bg_color', '#272424');
-    root.style.setProperty('--fg_color', '#F5F5F5');
+    root.style.setProperty('--bg_color_1', getComputedStyle(root).getPropertyValue('--bg_dark_1'));
+    root.style.setProperty('--bg_color_2', getComputedStyle(root).getPropertyValue('--bg_dark_2'));
+    root.style.setProperty('--fg_color', getComputedStyle(root).getPropertyValue('--fg_dark'));
 
     arrows.forEach(arrow => {
       arrow.src = 'assets/white_arrow.svg';
@@ -18,8 +19,9 @@ function changeTheme(checkbox) {
   
   else {
     // Light
-    root.style.setProperty('--bg_color', '#F5F5F5');
-    root.style.setProperty('--fg_color', '#464646');
+    root.style.setProperty('--bg_color_1', getComputedStyle(root).getPropertyValue('--bg_light_1'));
+    root.style.setProperty('--bg_color_2', getComputedStyle(root).getPropertyValue('--bg_light_2'));
+    root.style.setProperty('--fg_color', getComputedStyle(root).getPropertyValue('--fg_light'));
     
     arrows.forEach(arrow => {
       arrow.src = 'assets/black_arrow.svg';

87d8cfb805f21b55ea9900103d3062b4a218caae
Lucas Gabriel <66486119+lucasgabriel07@users.noreply.github.com>
2022-03-06 16:03:06 -0300

    Formulário de contato

---
+++ js/form.js
@@ -0,0 +1,28 @@
+const camposForm = document.querySelectorAll('.contato__formulario__campo');
+
+camposForm.forEach(campo => {
+  const label = campo.querySelector('label');
+  let input = campo.querySelector('input');
+  
+  if (!input) {
+    input = campo.querySelector('textarea');
+  }
+
+  const placeholder = input.placeholder;
+  
+  input.onfocus = () => {
+    label.style.display = 'block';
+    label.style.color = '#2A7AE4';
+    input.placeholder = '';
+    campo.style.borderBottom = '2px solid #2A7AE4';
+  }
+  
+  input.onblur = () => {
+    if (input.value == '') {
+      label.style.display = 'none';
+      input.placeholder = placeholder;
+    }
+    label.style.color = '#A2A2A2';
+    campo.style.borderBottom = '';
+  }
+});
